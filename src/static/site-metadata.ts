22014270f8f81d60614900bb2d8e614dcf3031da
laqieer <laqieer@126.com>
2024-05-15 22:48:47 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -10,14 +10,18 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
-  siteUrl: 'https://laqieer.github.io/running_page',
+  siteTitle: 'Home Page',
+  siteUrl: 'https://laqieer.github.io',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal running page',
   navLinks: [
     {
       name: 'Home',
       url: 'https://laqieer.github.io/running.html',
+    },
+    {
+      name: 'Strava',
+      url: 'https://www.strava.com/athletes/136949100',
     },
     {
       name: 'About',
